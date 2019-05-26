c0fad11e5604ae354865db427f5d305f64af3376
Slothsoft <admin@slothsoft.de>
2019-05-26 11:21:38 +0200

    initial commit with image based letters

--- core/src/test/java/de/slothsoft/shera/PhoneticSoundTest.java
+++ core/src/test/java/de/slothsoft/shera/PhoneticSoundTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
